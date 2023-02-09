9b0d1350fca0f022c24c2df1ae6657a001f7de2d
HanMengYao-GameEngine <43630878+HanMengYao-GameEngine@users.noreply.github.com>
2023-02-09 19:04:33 +0800

    DirectionalShadows

---
+++ DirectionalShadows/Assets/CusotmRP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+    static int baseColorId = Shader.PropertyToID("_BaseColor");
+    static int cutoffId = Shader.PropertyToID("_Cutoff");
+    static int metallicId = Shader.PropertyToID("_Metallic");
+    static int smoothnessId = Shader.PropertyToID("_Smoothness");
+    static MaterialPropertyBlock block;
+
+    [SerializeField]
+    Color baseColor = Color.white;
+    [SerializeField, Range(0f, 1f)]
+    float cutoff = 0.5f;
+    //定义金属度和光滑度
+    [SerializeField, Range(0f, 1f)]
+    float metallic = 0f;
+    [SerializeField, Range(0f, 1f)]
+    float smoothness = 0.5f;
+
+    void OnValidate()
+    {
+        if (block == null)
+        {
+           block = new MaterialPropertyBlock(); 
+        }
+        //设置材质属性
+        block.SetColor(baseColorId, baseColor);
+        block.SetFloat(cutoffId, cutoff);
+        block.SetFloat(metallicId, metallic);
+        block.SetFloat(smoothnessId, smoothness);
+        GetComponent<Renderer>().SetPropertyBlock(block);
+    }
+    void Awake()
+    {
+        OnValidate();
+    }
+}

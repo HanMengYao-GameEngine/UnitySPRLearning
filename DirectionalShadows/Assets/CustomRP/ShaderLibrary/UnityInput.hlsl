4336cf567ff8596712cf7645b6b6d0ec3a82a9f6
HanMengYao-GameEngine <43630878+HanMengYao-GameEngine@users.noreply.github.com>
2023-02-11 21:30:30 +0800

    DirectionalShadows

---
+++ DirectionalShadows/Assets/CustomRP/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,20 @@
+﻿//unity标准输入库
+#ifndef CUSTOM_UNITY_INPUT_INCLUDED
+#define CUSTOM_UNITY_INPUT_INCLUDED
+CBUFFER_START(UnityPerDraw)
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+float4 unity_LODFade;
+//相机位置
+float3 _WorldSpaceCameraPos;
+//这个矩阵包含一些在这里我们不需要的转换信息
+real4 unity_WorldTransformParams;
+float4x4 unity_Prev_MatrixM;
+float4x4 unity_Prev_WorldToObject;
+CBUFFER_END
+
+
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+#endif

f0baea9c556596bcfe8ebfbd792a85c19eb22b2a
HanMengYao-GameEngine <43630878+HanMengYao-GameEngine@users.noreply.github.com>
2023-02-19 20:34:34 +0800

    Particles

---
+++ Particles/Assets/CustomRP/ShaderLibrary/Common.hlsl
@@ -0,0 +1,54 @@
+﻿//公共方法库
+#ifndef CUSTOM_COMMON_INCLUDED
+#define CUSTOM_COMMON_INCLUDED
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonMaterial.hlsl"
+#include "UnityInput.hlsl"
+//定义一些宏取代常用的转换矩阵
+#define UNITY_MATRIX_M unity_ObjectToWorld
+#define UNITY_MATRIX_I_M unity_WorldToObject
+#define UNITY_MATRIX_V unity_MatrixV
+#define UNITY_MATRIX_VP unity_MatrixVP
+#define UNITY_MATRIX_P glstate_matrix_projection
+
+
+//获取值的平方
+float Square (float v) {
+	return v * v;
+}
+//计算两点间距离的平方
+float DistanceSquared(float3 pA, float3 pB) {
+	return dot(pA - pB, pA - pB);
+}
+void ClipLOD (float2 positionCS, float fade) {
+	#if defined(LOD_FADE_CROSSFADE)
+		float dither = InterleavedGradientNoise(positionCS.xy, 0);
+		clip(fade + (fade < 0.0 ? dither : -dither));
+	#endif
+}
+
+#if defined(_SHADOW_MASK_ALWAYS) || defined(_SHADOW_MASK_DISTANCE)
+	#define SHADOWS_SHADOWMASK
+#endif
+
+#define UNITY_PREV_MATRIX_M unity_Prev_MatrixM
+#define UNITY_PREV_MATRIX_I_M unity_Prev_WorldToObject
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/UnityInstancing.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/SpaceTransforms.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Packing.hlsl"
+
+//解码法线数据，得到原来的法线向量
+float3 DecodeNormal (float4 sample, float scale) {
+	#if defined(UNITY_NO_DXT5nm)
+	return UnpackNormalRGB(sample, scale);
+	#else
+	return UnpackNormalmapRGorAG(sample, scale);
+	#endif
+}
+//将法线从切线空间转换到世界空间
+float3 NormalTangentToWorld (float3 normalTS, float3 normalWS, float4 tangentWS) {
+	//构建切线到世界空间的转换矩阵，需要:世界空间的法线，世界空间的切线的XYZ和W分量
+	float3x3 tangentToWorld = CreateTangentToWorld(normalWS, tangentWS.xyz, tangentWS.w);
+	return TransformTangentToWorld(normalTS, tangentToWorld);
+}
+#endif
